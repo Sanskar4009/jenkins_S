65bb87545061b348b66befc4049e79ebf6633804
Sanskar4009 <sanskarsainik40@gmail.com>
2026-08-03 15:15:46 +0530

    First commit

---
+++ app.java
@@ -0,0 +1,9 @@
+import java.util.* ;
+public class app{
+    @SuppressWarnings("all")
+
+    public static void main(Stringp[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hii Hello settle down");
+    }
+}
