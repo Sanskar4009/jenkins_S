2f24929feaf62baa47e9d4fb69e05b366998c543
Sanskar4009 <sanskarsainik40@gmail.com>
2026-08-03 15:27:56 +0530

    3 commit

--- app.java
+++ app.java
@@ -5,5 +5,6 @@ public class app{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Hii Hello settle down");
+        System.out.println("Hii Hello settle down 1");
     }
 }
