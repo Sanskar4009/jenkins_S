8475cb10d5469bbe9ecdfb5be74587ae189d7c15
Sanskar4009 <sanskarsainik40@gmail.com>
2026-08-03 15:20:56 +0530

    make some change in the code:

--- app.java
+++ app.java
@@ -2,7 +2,7 @@ import java.util.* ;
 public class app{
     @SuppressWarnings("all")
 
-    public static void main(Stringp[] args){
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Hii Hello settle down");
     }
